2f3098ea492ceee717ea36f7238cd334420e40b3
SumonWebDev <sumon.mathjnu@gmail.com>
2020-10-10 23:50:40 +0600

    sending file

--- src/App.js
+++ src/App.js
@@ -12,12 +12,16 @@ function App() {
     .catch(error=>console.log(error))
 
   },[])
+  const handleAddCountry=(country)=>{
+    console.log('country added',country)
+  }
   return (
     <div className="App">
     <h3>loading country:{countries.length}</h3>
+    <h4>country added</h4>
     <ul>
       {
-        countries.map(country=><Country country={country} key={country.alpha3Code}></Country>)
+        countries.map(country=><Country country={country} handleAddCountry={handleAddCountry} key={country.alpha3Code}></Country>)
       }
     </ul>
       <header className="App-header">
--- src/componants/Country/Country.js
+++ src/componants/Country/Country.js
@@ -3,12 +3,15 @@ import React from 'react';
 const Country = (props) => {
     const {name,population,region,flag}=props.country;
     const flagStyle={height:'70px'}
+const countryStyle={border:'1px solid green', margin:'10px', padding:'10px'}
+const handleAddCountry=props.handleAddCountry;
     return (
-        <div>
-            <h4>This is a Country : {props.country.name}</h4>
+        <div style={countryStyle}>
+            <h2>{props.country.name}</h2>
             <img style={flagStyle} src={flag}/>
             <p>population:{props.country.population}</p>
             <p>region:{props.country.region}</p>
+            <button onClick={()=>handleAddCountry(props.country)}>Add Country</button>
         </div>
     );
 };
